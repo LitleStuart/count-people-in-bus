9839f6d2eed4ad1c80da1031136f46839b0ad119
Ivan Mukhorin <mukhorinvanga@gmail.com>
2022-11-10 21:13:31 +0500

    wip

--- src/components/PlayScene/index.tsx
+++ src/components/PlayScene/index.tsx
@@ -4,24 +4,29 @@ import styles from "./PlayScene.module.scss";
 import video from "../../resources/1.mov";
 
 const PlayScene = () => {
+  const videoRef = React.useRef<HTMLVideoElement>(null);
   const [count, setCount] = React.useState(9);
   const [videoIsPlaying, setVideoIsPlaying] = React.useState(false);
   const [speed, setSpeed] = React.useState(2);
-  const videoRef = React.useRef<HTMLVideoElement>(null);
+  const [withMovement, setWithMovement] = React.useState(true);
+  const [linksToVideoTime, setlinksToVideoTime] = React.useState<number[]>([]);
 
   React.useEffect(() => {
     videoRef.current!.playbackRate = speed;
-    console.log(videoRef.current!.playbackRate);
   }, [speed]);
 
-  const toggleVideo = () => {
+  React.useEffect(() => {
+    setlinksToVideoTime([1, 5, 9, 15]);
+  }, []);
+
+  React.useEffect(() => {
     if (videoIsPlaying) {
-      videoRef.current?.pause();
-    } else {
       videoRef.current?.play();
+    } else {
+      videoRef.current?.pause();
     }
-    setVideoIsPlaying(!videoIsPlaying);
-  };
+  }, [withMovement, videoIsPlaying]);
+
   const toggleVideoSpeed = () => {
     if (speed === 3) {
       setSpeed(1);
@@ -37,14 +42,36 @@ const PlayScene = () => {
       setCount(count - 1);
     }
   };
+  const setCurrentVideoTime = (secs: number) => {
+    videoRef.current!.currentTime = secs;
+  };
 
   return (
     <div className={styles.container}>
       <h2>People: {count}</h2>
       <div className={styles.col}>
-        <Link to="/" className={`${styles.button} ${styles.controlsButton}`}>
-          <span className="material-icons-round">close</span>
-        </Link>
+        <div>
+          <Link to="/" className={`${styles.button} ${styles.controlsButton}`}>
+            <span className="material-icons-round">close</span>
+          </Link>
+          <div style={{ textAlign: "center" }}>
+            {linksToVideoTime.map((val) => {
+              return (
+                <p
+                  key={val}
+                  className={styles.link}
+                  onClick={() => {
+                    setCurrentVideoTime(val);
+                  }}
+                >{`${Math.floor(val / 60)
+                  .toString()
+                  .padStart(2, "0")}:${(val % 60)
+                  .toString()
+                  .padStart(2, "0")}`}</p>
+              );
+            })}
+          </div>
+        </div>
         <div
           className={`${styles.button} ${styles.countButton}`}
           onClick={decrementCount}
@@ -54,14 +81,23 @@ const PlayScene = () => {
       </div>
       <div className={styles.col}>
         <div className={styles.videoMask}>
-          <video ref={videoRef} src={video} className={styles.player}></video>
+          <video
+            ref={videoRef}
+            src={video}
+            className={styles.player}
+            onEnded={() => {
+              setVideoIsPlaying(false);
+            }}
+          ></video>
         </div>
       </div>
       <div className={styles.col}>
         <div>
           <div
             className={`${styles.button} ${styles.controlsButton}`}
-            onClick={toggleVideo}
+            onClick={() => {
+              setVideoIsPlaying(!videoIsPlaying);
+            }}
           >
             <span className="material-icons-round">
               {videoIsPlaying ? "pause" : "play_arrow"}
@@ -74,6 +110,17 @@ const PlayScene = () => {
           >
             <span style={{ fontSize: "2em", lineHeight: "calc(1em*1.5)" }}>
               {`${speed}x`}
+            </span>
+          </div>
+          <div
+            className={`${styles.button} ${styles.controlsButton}`}
+            style={{ marginTop: 10 }}
+            onClick={() => {
+              setWithMovement(!withMovement);
+            }}
+          >
+            <span className="material-icons-round">
+              {withMovement ? "directions_run" : "airline_seat_recline_normal"}
             </span>
           </div>
         </div>
