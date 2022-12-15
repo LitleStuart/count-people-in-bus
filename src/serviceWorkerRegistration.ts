59b2708b5ed0e57093a09ad1dd092f053e1471f3
Ivan Mukhorin <mukhorinvanga@gmail.com>
2022-12-15 20:20:58 +0500

    making pwa

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -20,9 +20,8 @@ export function register(config?: Config) {
     if (publicUrl.origin !== window.location.origin) {
       return;
     }
-
     window.addEventListener("load", () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.ts`;
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
