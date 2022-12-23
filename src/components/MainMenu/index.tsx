85461e03df22b5bd6708f1004ef0008c602f67b9
Ivan Mukhorin <mukhorinvanga@gmail.com>
2022-12-23 13:35:47 +0500

    add force landscape on iOS

--- src/components/MainMenu/index.tsx
+++ src/components/MainMenu/index.tsx
@@ -1,7 +1,25 @@
+import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./MainMenu.module.scss";
 
 const MainMenu = () => {
+  const [orientation, setOrientation] = React.useState(0);
+  const getOrientation = () => {
+    return window.orientation;
+  };
+  window.addEventListener("load", () => {
+    setOrientation(getOrientation());
+  });
+  window.addEventListener("orientationchange", () => {
+    setOrientation(getOrientation());
+  });
+  if (orientation === 0) {
+    return (
+      <div className={styles.container}>
+        <h1 className={styles.title}>Пожалуйста, переверните телефон</h1>
+      </div>
+    );
+  }
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Сколько людей в автобусе?</h1>
