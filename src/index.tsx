591f90c0a994406d5ffba8168d5753ab116e7cba
Ivan Mukhorin <mukhorinvanga@gmail.com>
2023-02-16 16:22:14 +0500

    remove service-worker (not useful for my work :( )

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import "./index.css";
 import App from "./App";
 
@@ -12,5 +11,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-serviceWorkerRegistration.register();
