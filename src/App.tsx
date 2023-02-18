ac90c3528667085d2a0a24e04ca009bb6e9cd9c8
Ivan Mukhorin <mukhorinvanga@gmail.com>
2023-02-18 14:31:04 +0500

    remove MainMenu

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,8 @@
-import { createHashRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
-import MainMenu from "./components/MainMenu";
 import PlayScene from "./components/PlayScene";
-const router = createHashRouter([
-  {
-    path: "/",
-    element: <MainMenu />,
-  },
-  {
-    path: "/start",
-    element: <PlayScene />,
-  },
-]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  return <PlayScene />;
 }
 
 export default App;
