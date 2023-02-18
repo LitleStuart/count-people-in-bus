f6eddd374e9765e2083d48a6f6dd41f51af52443
Ivan Mukhorin <mukhorinvanga@gmail.com>
2023-02-18 14:31:36 +0500

    remove intervals

--- src/components/PlayScene/index.tsx
+++ src/components/PlayScene/index.tsx
@@ -1,41 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import styles from "./PlayScene.module.scss";
 import { Player } from "./Player";
 import { MaterialButton } from "../MaterialButton";
 import Swal from "sweetalert2";
 
-const sendData = (
-  insidePeople: number,
-  outsidePeople: number,
-  intervals: number[]
-) => {
+const sendData = (insidePeople: number, outsidePeople: number) => {
   Swal.fire("Данные успешно отправлены", "", "success");
 };
 
-const getVideo = () => {
-  return "https://vpp-reg.tahoplan.ru/video/2022-12-29T13/001241dbf441_2022-12-29T13:29:27_007.mp4";
+const getVideoUrl = (): string => {
+  // @ts-ignore
+  return apex.item("P154_VIDEO").getValue();
 };
 
 const PlayScene = () => {
   const videoRef = React.useRef<HTMLVideoElement>(null);
+  const [videoUrl, setVideoUrl] = React.useState(getVideoUrl());
   const [insidePeople, setInsidePeople] = React.useState(0);
   const [outsidePeople, setOutsidePeople] = React.useState(0);
   const [videoIsPlaying, setVideoIsPlaying] = React.useState(false);
   const [speed, setSpeed] = React.useState(2);
-  const [intervals, setIntervals] = React.useState<number[]>([]);
 
   React.useEffect(() => {
     videoRef.current!.playbackRate = speed;
   }, [speed]);
-
-  React.useEffect(() => {
-    const SECONDS_FROM_START_VIDEO =
-      Math.round(videoRef.current!.currentTime * 1000) / 1000;
-    setIntervals([...intervals, SECONDS_FROM_START_VIDEO]);
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [insidePeople, outsidePeople]);
 
   React.useEffect(() => {
     if (videoIsPlaying) {
@@ -54,14 +42,14 @@ const PlayScene = () => {
   const decrementCount = () => {
     setOutsidePeople(outsidePeople + 1);
   };
+  const updateData = (): void => {
+    setInsidePeople(0);
+    setOutsidePeople(0);
+    setVideoUrl(getVideoUrl());
+  };
 
   const confirmData = () => {
     const confirmMessage = `Зашли ${insidePeople}, вышли ${outsidePeople}`;
-    const data = {
-      insidePeople: insidePeople,
-      outsidePeople: outsidePeople,
-      intervals: intervals,
-    };
     Swal.fire({
       title: "Подтверждение",
       text: confirmMessage,
@@ -72,10 +60,8 @@ const PlayScene = () => {
       confirmButtonColor: "#9086ff",
     }).then((result) => {
       if (result.isConfirmed) {
-        sendData(data.insidePeople, data.outsidePeople, data.intervals);
-        setInsidePeople(0);
-        setOutsidePeople(0);
-        setIntervals([]);
+        sendData(insidePeople, outsidePeople);
+        updateData();
       }
     });
   };
@@ -87,9 +73,6 @@ const PlayScene = () => {
       </h2>
       <div className={styles.col}>
         <div>
-          <Link to="/" className={`${styles.button} ${styles.controlsButton}`}>
-            <span className="material-icons-round">close</span>
-          </Link>
           <MaterialButton
             className={`${styles.button} ${styles.controlsButton}`}
             icon="done"
@@ -106,7 +89,7 @@ const PlayScene = () => {
       </div>
       <Player
         videoRef={videoRef}
-        source={getVideo()}
+        source={videoUrl}
         handleVideoEnd={() => {
           setVideoIsPlaying(false);
         }}
