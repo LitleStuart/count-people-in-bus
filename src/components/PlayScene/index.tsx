b907fb130e73a04dd1d185d77af631e12b411065
Ivan Mukhorin <mukhorinvanga@gmail.com>
2023-02-20 15:45:42 +0500

    some refactoring now can send data

--- src/components/PlayScene/index.tsx
+++ src/components/PlayScene/index.tsx
@@ -5,12 +5,30 @@ import { MaterialButton } from "../MaterialButton";
 import Swal from "sweetalert2";
 
 const sendData = (insidePeople: number, outsidePeople: number) => {
-  Swal.fire("Данные успешно отправлены", "", "success");
+  // @ts-ignore
+  apex.server
+    .process("send_data", {
+      // @ts-ignore
+      x01: apex.item("P154_ID_VIDEO").getValue(),
+      x02: insidePeople,
+      x03: outsidePeople,
+    })
+    .then(() => {
+      Swal.fire("Данные успешно отправлены", "", "success");
+    });
 };
 
 const getVideoUrl = (): string => {
   // @ts-ignore
   return apex.item("P154_VIDEO").getValue();
+};
+
+const getSpeedIcon = (speed: number) => {
+  return speed === 1 ? "chevron_right" : "keyboard_double_arrow_right";
+};
+
+const getPlayPauseIcon = (videoIsPlaying: boolean) => {
+  return videoIsPlaying ? "pause" : "play_arrow";
 };
 
 const PlayScene = () => {
@@ -42,10 +60,25 @@ const PlayScene = () => {
   const decrementCount = () => {
     setOutsidePeople(outsidePeople + 1);
   };
+
   const updateData = (): void => {
     setInsidePeople(0);
     setOutsidePeople(0);
-    setVideoUrl(getVideoUrl());
+    // @ts-ignore
+    apex.server
+      .process("get_new_video", {
+        // @ts-ignore
+        x01: apex.item("P154_BEG_DT").getValue(),
+        // @ts-ignore
+        x02: apex.item("P154_END_DT").getValue(),
+        // @ts-ignore
+        x03: apex.item("P154_VEH").getValue(),
+      })
+      .then((data: { id: number; url: string }) => {
+        // @ts-ignore
+        apex.item("P154_ID_VIDEO").setValue(data.id);
+        setVideoUrl(data.url);
+      });
   };
 
   const confirmData = () => {
@@ -56,12 +89,29 @@ const PlayScene = () => {
       showConfirmButton: true,
       confirmButtonText: "Да",
       showCancelButton: true,
-      cancelButtonText: "Нет",
+      cancelButtonText: "Отмена",
       confirmButtonColor: "#9086ff",
     }).then((result) => {
       if (result.isConfirmed) {
         sendData(insidePeople, outsidePeople);
         updateData();
+      }
+    });
+  };
+
+  const handleVideoEnd = () => {
+    setVideoIsPlaying(false);
+    Swal.fire({
+      title: "Подтверждение",
+      text: "Видео закончилось, хотите сразу отправить данные?",
+      showConfirmButton: true,
+      confirmButtonText: "Да",
+      confirmButtonColor: "#9086ff",
+      showCancelButton: true,
+      cancelButtonText: "Отмена",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        confirmData();
       }
     });
   };
@@ -81,37 +131,31 @@ const PlayScene = () => {
         </div>
         <MaterialButton
           className={`${styles.button} ${styles.countButton}`}
-          handleClick={() => {
-            decrementCount();
-          }}
+          handleClick={decrementCount}
           icon="remove"
         />
       </div>
       <Player
         videoRef={videoRef}
         source={videoUrl}
-        handleVideoEnd={() => {
-          setVideoIsPlaying(false);
-        }}
+        handleVideoEnd={handleVideoEnd}
       />
       <div className={styles.col}>
         <div>
           <MaterialButton
             handleClick={() => setVideoIsPlaying(!videoIsPlaying)}
-            icon={videoIsPlaying ? "pause" : "play_arrow"}
+            icon={getPlayPauseIcon(videoIsPlaying)}
             className={`${styles.button} ${styles.controlsButton}`}
           />
           <MaterialButton
             className={`${styles.button} ${styles.controlsButton}`}
-            handleClick={() => toggleVideoSpeed()}
-            icon={speed === 1 ? "chevron_right" : "keyboard_double_arrow_right"}
+            handleClick={toggleVideoSpeed}
+            icon={getSpeedIcon(speed)}
           />
         </div>
         <MaterialButton
           className={`${styles.button} ${styles.countButton}`}
-          handleClick={() => {
-            incrementCount();
-          }}
+          handleClick={incrementCount}
           icon="add"
         />
       </div>
