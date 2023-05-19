487bd378d4fe44681161da05a4cd4c338850d96e
Ivan Mukhorin <mukhorinvanga@gmail.com>
2023-05-19 16:52:38 +0500

    improve UI with useGesture

--- src/components/PlayScene/index.tsx
+++ src/components/PlayScene/index.tsx
@@ -205,10 +205,6 @@ const PlayScene = () => {
         videoRef={videoRef}
         source={videoUrl}
         handleVideoEnd={handleVideoEnd}
-        handlePlayPause={() => {
-          setVideoIsPlaying(!videoRef.current?.paused);
-        }}
-        handleRateChange={() => (videoRef.current!.playbackRate = speed)}
         handleTimeUpdate={() => {
           setVideoTime({
             duration: videoTime.duration,
